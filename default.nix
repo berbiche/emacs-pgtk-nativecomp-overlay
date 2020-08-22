bd7742291cfdfcb6138479cef0f68dba1503a99a
Michael Lingelbach <m.j.lbach@gmail.com>
2020-08-22 16:39:29 -0700

    Update

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ pkgs ? import <nixos-unstable> { } }:
+{ pkgs ? import <nixos-unstable> {} }:
 let
   rev = "b7adb08f960fe6568f702b8f328e65e3833ffc13";
   sha256 = "0p852k5wf8sy9h7x2z6iivf9xnhpy85vly9fn0a1qj2japrhvyr2";
 in
 {
-  ci = (import ./nix { }).ci;
+  ci = (import ./nix {}).ci;
   emacsGccPGtk = builtins.foldl' (drv: fn: fn drv)
     pkgs.emacs
     [
@@ -23,20 +23,23 @@ in
             };
 
             configureFlags = old.configureFlags
-              ++ [ "--with-pgtk" ];
+            ++ [ "--with-pgtk" ];
 
 
             patches = [
-              (pkgs.fetchpatch {
-                name = "clean-env.patch";
-                url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/clean-env.patch";
-                sha256 = "0lx9062iinxccrqmmfvpb85r2kwfpzvpjq8wy8875hvpm15gp1s5";
-              })
-              (pkgs.fetchpatch {
-                name = "tramp-detect-wrapped-gvfsd.patch";
-                url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/tramp-detect-wrapped-gvfsd.patch";
-                sha256 = "19nywajnkxjabxnwyp8rgkialyhdpdpy26mxx6ryfl9ddx890rnc";
-              }
+              (
+                pkgs.fetchpatch {
+                  name = "clean-env.patch";
+                  url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/clean-env.patch";
+                  sha256 = "0lx9062iinxccrqmmfvpb85r2kwfpzvpjq8wy8875hvpm15gp1s5";
+                }
+              )
+              (
+                pkgs.fetchpatch {
+                  name = "tramp-detect-wrapped-gvfsd.patch";
+                  url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/tramp-detect-wrapped-gvfsd.patch";
+                  sha256 = "19nywajnkxjabxnwyp8rgkialyhdpdpy26mxx6ryfl9ddx890rnc";
+                }
               )
             ];
 
