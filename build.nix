ebae7ad2673e86cade3ea6cd0b3765a01b534516
Michael Lingelbach <m.j.lbach@gmail.com>
2020-08-23 11:09:19 -0700

    Switch to overlay format

---
+++ build.nix
@@ -0,0 +1,10 @@
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = sources."nixos-unstable";
+  emacs-pgtk-overlay = import ./default.nix;
+  pkgs = import nixpkgs { config = {}; overlays = [ emacs-pgtk-overlay ]; };
+in
+{
+  emacsGccPgtk = pkgs.emacsGccPgtk;
+  emacsGccPgtkWrapped = pkgs.emacsGccPgtkWrapped;
+}
