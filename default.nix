d9458fff0f25b18125418d213554f6c710d48aa5
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-08-30 22:56:00 -0400

    Flake init

--- default.nix
+++ default.nix
@@ -1,60 +1,14 @@
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources."nixos-unstable" { }
+, emacs-pgtk-nativecomp ? sources."emacs-pgtk-nativecomp"
+}:
+
 let
-  sources = import ./nix/sources.nix;
-  nixpkgs = sources."nixos-unstable";
-  pkgs' = import nixpkgs {};
-  emacs-pgtk-nativecomp = sources."emacs-pgtk-nativecomp";
-
-  emacsGccPgtk =
-    { pkgs ? pkgs' }:
-    builtins.foldl' (drv: fn: fn drv) pkgs.emacs [
-
-      (drv: drv.override { srcRepo = true; })
-
-      (
-        drv: drv.overrideAttrs (
-          old: {
-            name = "emacsGccPgtk";
-            version = "28.0.50";
-            src = pkgs.fetchFromGitHub {
-              inherit (emacs-pgtk-nativecomp) owner repo rev sha256;
-            };
-
-            configureFlags = old.configureFlags
-            ++ [ "--with-pgtk" ];
-
-
-            patches = [
-              (
-                pkgs.fetchpatch {
-                  name = "clean-env.patch";
-                  url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/clean-env.patch";
-                  sha256 = "0lx9062iinxccrqmmfvpb85r2kwfpzvpjq8wy8875hvpm15gp1s5";
-                }
-              )
-              (
-                pkgs.fetchpatch {
-                  name = "tramp-detect-wrapped-gvfsd.patch";
-                  url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/tramp-detect-wrapped-gvfsd.patch";
-                  sha256 = "19nywajnkxjabxnwyp8rgkialyhdpdpy26mxx6ryfl9ddx890rnc";
-                }
-              )
-            ];
-
-            postPatch = old.postPatch + ''
-              substituteInPlace lisp/loadup.el \
-              --replace '(emacs-repository-get-version)' '"${emacs-pgtk-nativecomp.rev}"' \
-              --replace '(emacs-repository-get-branch)' '"master"'
-            '';
-
-          }
-        )
-      )
-      (
-        drv: drv.override {
-          nativeComp = true;
-        }
-      )
-    ];
+  libPath = with pkgs; lib.concatStringsSep ":" [
+    "${lib.getLib libgccjit}/lib/gcc/${stdenv.targetPlatform.config}/${libgccjit.version}"
+    "${lib.getLib stdenv.cc.cc}/lib"
+    "${lib.getLib stdenv.glibc}/lib"
+  ];
 in
 rec {
   ci = (import ./nix {}).ci;
@@ -62,16 +16,58 @@ rec {
   overlay = final: prev: builtins.mapAttrs (x: x prev) packages;
 
   packages = {
-    inherit emacsGccPgtk;
+    emacsGccPgtk = 
+      builtins.foldl' (drv: fn: fn drv) pkgs.emacs [
+
+        (drv: drv.override { srcRepo = true; })
+
+        (
+          drv: drv.overrideAttrs (
+            old: {
+              name = "emacsGccPgtk";
+              version = "28.0.50";
+              src = pkgs.fetchFromGitHub {
+                inherit (emacs-pgtk-nativecomp) owner repo rev sha256;
+              };
+
+              configureFlags = old.configureFlags
+              ++ [ "--with-pgtk" ];
+
+
+              patches = [
+                (
+                  pkgs.fetchpatch {
+                    name = "clean-env.patch";
+                    url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/clean-env.patch";
+                    sha256 = "0lx9062iinxccrqmmfvpb85r2kwfpzvpjq8wy8875hvpm15gp1s5";
+                  }
+                )
+                (
+                  pkgs.fetchpatch {
+                    name = "tramp-detect-wrapped-gvfsd.patch";
+                    url = "https://raw.githubusercontent.com/nix-community/emacs-overlay/master/patches/tramp-detect-wrapped-gvfsd.patch";
+                    sha256 = "19nywajnkxjabxnwyp8rgkialyhdpdpy26mxx6ryfl9ddx890rnc";
+                  }
+                )
+              ];
+
+              postPatch = old.postPatch + ''
+                substituteInPlace lisp/loadup.el \
+                --replace '(emacs-repository-get-version)' '"${emacs-pgtk-nativecomp.rev}"' \
+                --replace '(emacs-repository-get-branch)' '"master"'
+              '';
+
+            }
+          )
+        )
+        (
+          drv: drv.override {
+            nativeComp = true;
+          }
+        )
+      ];
 
     emacsGccPgtkWrapped =
-      { pkgs ? pkgs' }: let
-        libPath = with pkgs; lib.concatStringsSep ":" [
-          "${lib.getLib libgccjit}/lib/gcc/${stdenv.targetPlatform.config}/${libgccjit.version}"
-          "${lib.getLib stdenv.cc.cc}/lib"
-          "${lib.getLib stdenv.glibc}/lib"
-          ];
-      in
       pkgs.symlinkJoin {
         name = "emacsGccPgtkWrapped";
         paths = [ (emacsGccPgtk pkgs) ];
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Emacs with native compilation and pgtk";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.emacs-pgtk-nativecomp = { url = "github:flatwhatson/emacs/pgtk-nativecomp"; flake = false; };
+
+  outputs = { self, flake-utils, emacs-pgtk-nativecomp, nixpkgs }:
+    (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      src = pkgs.callPackage ./default.nix { inherit emacs-pgtk-nativecomp; };
+    in rec {
+      inherit src packages;
+      legacyPackages = packages;
+      defaultPackage = packages.emacsGccPgtkWrapped;
+    })
+    // {
+      overlay = import ./overlay.nix;
+    };
+}
